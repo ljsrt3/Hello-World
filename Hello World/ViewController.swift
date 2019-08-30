176d8756e3a2dee2281a956191d6349a43f9ab8f
Scott, Lindsey (MU-Student) <ljsrt3@laffc1205-mac49.local>
2019-08-30 17:15:15 -0500

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Scott, Lindsey (MU-Student) on 8/30/19.
+//  Copyright © 2019 Scott, Lindsey (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
